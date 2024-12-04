1592d41dfde5c955b30a748faf86cdced752c695
shahid <shahidshah77562@gmail.com>
2024-12-04 09:34:54 +0530

    Add files via upload

---
+++ real.java
@@ -0,0 +1,10 @@
+public class real {
+    public static void main(String[] args) {
+        int a=12;
+        int b=14;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+    }
+    
+}
